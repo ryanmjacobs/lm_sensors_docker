b17ccfda76c9bd551682ea0ff45423a85f6a6c49
Ryan Jacobs <ryan@rmj.us>
2019-12-26 21:50:06 -0800

    sensors json

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM alpine
 RUN apk add lm_sensors
+COPY push.sh /
 CMD ["sensors", "--json"]
---
+++ push.sh
@@ -0,0 +1,7 @@
+run() {
+    cmd="$1"
+    content="$(eval "$cmd")"
+    psql <<< "INSERT INTO acpi_log (host, cmd, content) VALUES ('$HOST', '$cmd', '$content')"
+}
+
+run 'sensors --json'
