41623ab369892c06aa43588823af9bbfa4393ec2
Ryan Jacobs <ryan@rmj.us>
2019-12-30 00:27:57 -0800

    include git sha in version

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 name=lm_sensors
-ver=0.0.6
+#ver=0.0.6
+ver="0.0.6-$(git rev-parse --short HEAD)"
 docker build -t "$name:$ver" .
 docker tag "$name:$ver" 10.0.10.20:5000/"$name:$ver"
 docker push 10.0.10.20:5000/"$name:$ver"
